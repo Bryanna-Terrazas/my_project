a10a18d5a815e2eea0f551691bef2b7fcc18c861
Jaggerjazz <157041735+Jaggerjazz@users.noreply.github.com>
2026-04-20 17:03:06 +0000

    Merge variables and if-else into one file

--- rust_basics/examples/if_else.rs
+++ rust_basics/examples/if_else.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let score = 70;
+    let score = 90;
 
     if score >= 90 {
         println!("Grade: A");
@@ -10,4 +10,16 @@ fn main() {
     } else {
         println!("Grade: F");
     }
+
+        // 1. Immutable variable (cannot be changed)
+    let school = "UTRGV";
+    
+    // 2. Mutable variable (can be changed)
+    let mut credits = 12;
+    credits = 18; // Changing the value
+    
+    // 3. Variable with a specific data type (Integer)
+    let year: i32 = 2026;
+    // 4. {} are place holders for the vairable. if not then a error will occur
+    println!("Welcome to {}, year {}. You have {} credits.", school, year, credits);
 }
